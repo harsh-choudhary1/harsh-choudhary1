169089ba2aa690111ba052c7cc93ac56ac91dfb3
harsh-choudhary1 <160221948+harsh-choudhary1@users.noreply.github.com>
2024-03-21 18:49:11 +0530

    Add files via upload

---
+++ pythonProject1/main.py
@@ -0,0 +1,26 @@
+# import os
+# def strftime():
+#     import time
+#     timeis = time.strftime(' %H,%M,%S')  # function bnana b sikh ka 21 tarik h aj
+#     print(timeis)
+#     # print(__name__)
+#
+#
+# if __name__ == '__main__':
+#     strftime()
+# command = f"say {strftime()}"
+# os.system(command)
+# import os
+# import time
+# if __name__ == '__main__':
+#     x = time.strftime('%H')
+#     y = time.strftime('%M')
+#     z = time.strftime('%s')
+#     print(x)
+#     os.system(f"say 'is time {x} bajj kar {y}minute hui ha'")
+import os
+import time
+if __name__=='__main__':
+    x = time.strftime('%Y-%m-%d'"&"'%H-%M-%S,%p')
+    print(x)
+    os.system(f"say ' the time is {x}' ")
